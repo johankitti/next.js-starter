b493539719866c815e5c9b531ad068ed7da0692d
johankitti <johan.kitti@gmail.com>
2019-02-20 12:29:38 +0100

    added custom express server with gzipping

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const compression = require('compression');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 8080;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.use(compression());
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
