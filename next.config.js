480514686d196c7bbd43122184b2df4989fae1b8
johankitti <johan.kitti@gmail.com>
2019-02-20 10:56:52 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,20 +1,29 @@
+const compose = require('next-compose');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass(
-  { cssModules: true },
-  withBundleAnalyzer({
-    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-    bundleAnalyzerConfig: {
-      server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html',
-      },
-      browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html',
-      },
+const sassConfig = { cssModules: true };
+const bundleAnalyzerConfig = {
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundle-analyze/server.html',
     },
-  }),
-);
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundle-analyze/client.html',
+    },
+  },
+};
+
+module.exports = compose([
+  [withSass, sassConfig],
+  [withBundleAnalyzer, bundleAnalyzerConfig],
+  {
+    webpack: config => {
+      return config;
+    },
+  },
+]);
