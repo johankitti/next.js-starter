3a724ceb8740c38110829988375c49244a647723
johankitti <johan.kitti@gmail.com>
2019-02-20 15:07:51 +0100

    ok can it evven be cooler?

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ import styles from './index.scss';
 class Home extends React.Component {
   static async getInitialProps({ reduxStore }) {
     // const isServer = !!req;
-    await reduxStore.dispatch(fetchShows('batman'));
+    await ['batman', 'aquaman', 'spiderman', 'x-men'].forEach(show => {
+      reduxStore.dispatch(fetchShows(show));
+    });
     return {};
   }
 
@@ -21,20 +23,22 @@ class Home extends React.Component {
     return (
       <div className={styles.test}>
         <MetaTags title="Some nice hooome pages bros" description="yo bro, this is THE page dude" />
-        {Object.keys(shows).map(showKey => (
-          <div key={showKey}>
-            <h2>{showKey}</h2>
-            {shows[showKey].map(({ show }) => (
-              <div key={show.id} className={styles.show}>
-                <div style={{ backgroundImage: `url(${show.image.medium})` }} className={styles.showImg} />
-                <div className={styles.text}>
-                  <span className={styles.title}>{show.name}</span>
-                  <span className={styles.desc}>{show.name}</span>
+        {Object.keys(shows)
+          .sort()
+          .map(showKey => (
+            <div key={showKey}>
+              <h1>{`${showKey[0].toUpperCase()}${showKey.slice(1)}`}</h1>
+              {shows[showKey].map(({ show }) => (
+                <div key={show.id} className={styles.show}>
+                  <div style={{ backgroundImage: `url(${show.image.medium})` }} className={styles.showImg} />
+                  <div className={styles.text}>
+                    <span className={styles.title}>{show.name}</span>
+                    <span className={styles.desc}>{show.name}</span>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          ))}
       </div>
     );
   }
